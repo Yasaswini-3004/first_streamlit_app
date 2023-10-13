cf0d49ae7c3bf8222707e03bbeb7c9b042917add
Yasaswini-3004 <54450118+Yasaswini-3004@users.noreply.github.com>
2023-10-13 20:20:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner");
 streamlit.header("Breakfast Menu");
-stremlit.text("Omega 3 and Blueberry oatmeal");
-stremlit.text("Kale, Spinach and Rocket Smoothie");
-stremlit.text("Hard-Boiled Free Range Egg");
+streamlit.text("Omega 3 and Blueberry oatmeal");
+streamlit.text("Kale, Spinach and Rocket Smoothie");
+streamlit.text("Hard-Boiled Free Range Egg");
